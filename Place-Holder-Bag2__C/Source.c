47568f12723377ee3147a934f68269c6c08a5255
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2020-11-06 21:40:26 +0700

    Add project files.

---
+++ Place-Holder-Bag2__C/Source.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+	char right[] = "right";
+	char left[] = "left";
+
+	printf("%20s\n", right);
+	printf("%-20s\n", left);
+
+	_getch();
+	return(0);
+}
